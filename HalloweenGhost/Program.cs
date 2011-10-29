9f34981438c68f1e82b6aef81566ae5acddd8cd3
Mark Miller <mark@marklio.com>
2011-10-28 21:55:47 -0700

    Adding the GhostCentral project.  Sweet project name, eh?

--- HalloweenGhost/Program.cs
+++ HalloweenGhost/Program.cs
@@ -12,22 +12,26 @@ namespace HalloweenGhost
 {
     public class Program
     {
-        static SerialPort _UART = new SerialPort("COM1");
+        static SerialPort _UART = new SerialPort("COM3", 115200);//, 115200, Parity.None, 8, StopBits.One);
         const int BUFFER_LENGTH = 256;
         static byte[] _Buffer = new byte[BUFFER_LENGTH];
         public static void Main()
         {
+            _UART.ErrorReceived += (s, e) =>
+            {
+                Debug.Print("ERROR:"+e.EventType.ToString());
+            };
             _UART.Open();
+
             bool ledState = false;
 
             OutputPort led = new OutputPort((Cpu.Pin)FEZ_Pin.Digital.LED, ledState);
 
-            var outBuffer = Encoding.UTF8.GetBytes("TEST");
+            var outBuffer = Encoding.UTF8.GetBytes("THIS IS A TEST OF THE EMERGENCY BROADCAST SYSTEM");
 
             while (true)
             {
                 // Sleep for 500 milliseconds
-                Thread.Sleep(500);
                 if (_UART.BytesToRead > 0)
                 {
                     var read = _UART.Read(_Buffer, 0, BUFFER_LENGTH);
@@ -36,7 +40,11 @@ namespace HalloweenGhost
                     ledState = !ledState;
                     led.Write(ledState);
                 }
-                _UART.Write(outBuffer, 0, outBuffer.Length);
+                if (_UART.BytesToWrite == 0)
+                {
+                    _UART.Write(outBuffer, 0, outBuffer.Length);
+                    _UART.Flush();
+                }
             }
         }
 
